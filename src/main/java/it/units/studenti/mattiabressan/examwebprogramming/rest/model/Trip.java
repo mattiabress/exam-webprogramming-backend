bbde7bf86f808ef0b79e45d481af392be87f5212
mattiabress <mati22b@gmail.com>
2022-07-20 22:57:20 +0200

    hibernete configuration test

--- src/main/java/it/units/studenti/mattiabressan/examwebprogramming/rest/model/Trip.java
+++ src/main/java/it/units/studenti/mattiabressan/examwebprogramming/rest/model/Trip.java
@@ -1,15 +1,22 @@
 package it.units.studenti.mattiabressan.examwebprogramming.rest.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+//import org.hibernate.annotations.GenericGenerator;
 @Entity
 @Table(name="trip")
 public class Trip {
     @Id
     @Column(name="id")
+    @GeneratedValue(generator="increment")
+    //@GenericGenerator(name="increment", strategy = "increment")
     private final int id;
     @Column(name="name")
     private final String name;
